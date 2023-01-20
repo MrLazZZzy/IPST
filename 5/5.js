3a82e0bcd666ce32f8c2d21c5ce3d4dafd86ddad
MrLazZZzy <104832458+MrLazZZzy@users.noreply.github.com>
2023-01-20 23:55:03 +0700

    Update 5.js

--- 5/5.js
+++ 5/5.js
@@ -2,4 +2,6 @@ let line = 'DDADSADASDAAADS';
 let array = line.split('')
 let uniqueLine = [...new Set (array)]
 let result = uniqueLine.toString()
+result = result.replace(/,/g, '');
 console.log(result)
+
