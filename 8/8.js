392be798931d65d16103c25dba40c6f879b3066c
MrLazZZzy <104832458+MrLazZZzy@users.noreply.github.com>
2023-01-21 01:42:33 +0700

    Update 8.js

--- 8/8.js
+++ 8/8.js
@@ -1,13 +1,13 @@
 let array1 = [[1,4,5],[1,3,4],[2,6]]
 let array2 = []
-let a
+let tmp
 
 for (let i = 0; i < array1.length; i++)
 {
     for (let j = 0; j < array1[i].length; j++)
     {
-        a = array1[i][j];
-        array2.push(a);
+        tmp = array1[i][j];
+        array2.push(tmp);
     }
 }
 
