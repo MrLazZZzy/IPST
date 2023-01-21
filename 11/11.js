b3fe4cb2a9d18f69b176504e840697c91b79bdce
MrLazZZzy <104832458+MrLazZZzy@users.noreply.github.com>
2023-01-21 22:09:01 +0700

    Update 11.js

--- 11/11.js
+++ 11/11.js
@@ -1,45 +1,30 @@
 class Worker {
-    #name;
-    #surname;
-    #rate;
-    #days;
     constructor(name, surname, rate, days) {
-        this.#name = name
-        this.#surname = surname
-        this.#rate = rate
-        this.#days = days
+        this.name = name
+        this.surname = surname
+        this.rate = rate
+        this.days = days
     }
     getName() {
-        console.log(this.#name)
+        console.log(this.name + ' ' + this.surname)
     }
-    getSurname() {
-        console.log(this.#surname)
+    getSalary() {
+        console.log('Зарплата', this.rate * this.days)
     }
-    getRate() {
-        console.log(this.#rate)
-    }
-    getDays() {
-        console.log(this.#days)
-    }
+
 }
 
 let Worker1 = new Worker('Роман', 'Яринский', 200, 14)
 Worker1.getName()
-Worker1.getSurname()
-Worker1.getRate()
-Worker1.getDays()
+Worker1.getSalary()
 
 let Worker2 = new Worker('Иван', 'Попоша', 0, 365)
 Worker2.getName()
-Worker2.getSurname()
-Worker2.getRate()
-Worker2.getDays()
+Worker2.getSalary()
 
 let Worker3 = new Worker('Илья', 'Сишарп', 14500, 30)
 Worker3.getName()
-Worker3.getSurname()
-Worker3.getRate()
-Worker3.getDays()
+Worker3.getSalary()
 
 
 
